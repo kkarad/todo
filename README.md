ad2b3c8dea7faebc559c2abf6579a5bbf61cd777
kkarad <kkarad@gmail.com>
2019-04-01 00:50:07 +0100

    add command with struct Todo

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # todo
 Todo cli application built in Rust
+
+* [x] implement add command
+* [ ] store todo list in a file
+* [ ] implement list command
+* [ ] implement done command
+* [ ] implement rename command
+* [ ] implement priority command
+* [ ] validate arguments and display error messages
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
+#[derive(Debug)]
+struct Todo {
+    title: String,
+    completed: bool
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let command: String = args[1].clone();
+    if command == "add" {
+        let new_todo_title = args[2].clone();
+        let new_todo = Todo{title: new_todo_title, completed: false};
+        println!("{:?}", new_todo)
+    }
 }
